64f1e169090add7db3338ae067ae6a995992293e
Melike Demir <demirmelike187@hotmail.com>
2025-05-27 16:59:00 +0300

    Separated Enviroment as a development and production

--- Prensantation/HepsiApi.WebApi/Program.cs
+++ Prensantation/HepsiApi.WebApi/Program.cs
@@ -12,6 +12,13 @@ namespace HepsiApi.WebApi
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
+            builder.Services.AddSwaggerGen();
+            var env = builder.Environment;
+
+            builder.Configuration.SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
 
             var app = builder.Build();
 
@@ -19,6 +26,9 @@ namespace HepsiApi.WebApi
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseCors();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseAuthorization();
