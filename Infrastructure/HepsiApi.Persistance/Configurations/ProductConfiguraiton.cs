f2a36a742dc9717679365f3c7a7194fc1365486b
Melike Demir <demirmelike187@hotmail.com>
2025-05-29 15:01:18 +0300

    Created Configuraiton classes and seed data

---
+++ Infrastructure/HepsiApi.Persistance/Configurations/ProductConfiguraiton.cs
@@ -0,0 +1,45 @@
+﻿using Bogus;
+using HepsiApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApi.Persistance.Configurations
+{
+    public class ProductConfiguraiton : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            Faker faker = new("tr");
+
+            Product product1 = new()
+            {
+                Id = 1,
+                Title = faker.Commerce.ProductName(),
+                Description = faker.Commerce.ProductDescription(),
+                BranId = 1,
+                Discount = faker.Random.Decimal(0, 10),
+                Price = faker.Finance.Amount(10, 10000),
+                IsDeleted=false,
+                CreateDate=DateTime.Now
+            };
+            Product product2 = new()
+            {
+                Id = 2,
+                Title = faker.Commerce.ProductName(),
+                Description = faker.Commerce.ProductDescription(),
+                BranId = 3,
+                Discount = faker.Random.Decimal(0, 10),
+                Price = faker.Finance.Amount(10, 10000),
+                IsDeleted = false,
+                CreateDate = DateTime.Now
+            };
+
+            builder.HasData(product1, product2);
+        }
+    }
+}
